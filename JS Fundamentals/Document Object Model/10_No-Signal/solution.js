5c0a549f11279e2f04fdece3fa6625706458e021
Miroslav Stoyanov <miroslav.b.stoyanov@gmail.com>
2019-01-24 11:30:31 +0200

    Document Object Model

--- JS Fundamentals/Document Object Model/10_No-Signal/solution.js
+++ JS Fundamentals/Document Object Model/10_No-Signal/solution.js
@@ -1,3 +1,13 @@
 function solve() {
-	//TODO
+	let noSignal = document.getElementById('exercise').children[0];
+	noSignal.style.position  = 'absolute';
+
+    function getRandomNumber(min, max) {
+        return Math.round(Math.random() * (max - min) + min);
+    }
+
+    setInterval(function() {
+        noSignal.style.left = `${getRandomNumber(1, 81)}%`;
+        noSignal.style.top = `${getRandomNumber(1, 45)}vh`;
+    }, 3000);
 }
