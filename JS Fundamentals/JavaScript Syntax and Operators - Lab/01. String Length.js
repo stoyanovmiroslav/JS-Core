bbf616a032fbb5682de659bf802e8ac85f94a9b2
Miroslav Stoyanov <miroslav.b.stoyanov@gmail.com>
2019-01-27 16:54:27 +0200

    JavaScript Syntax and Operators - Lab

---
+++ JS Fundamentals/JavaScript Syntax and Operators - Lab/01. String Length.js
@@ -0,0 +1,7 @@
+function solve(arg1, arg2, arg3) {
+    let sumLength = arg1.length + arg2.length + arg3.length;
+    let averageLength = Math.round(sumLength / 3);
+
+    console.log(sumLength);
+    console.log(averageLength);
+}
